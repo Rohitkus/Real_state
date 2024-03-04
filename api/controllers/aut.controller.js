29041eb444e7e3808400712c7793eac2ff25927d
Rohitkus <79927890+Rohitkus@users.noreply.github.com>
2024-03-04 20:23:54 +0530

    create signin api route

--- api/controllers/aut.controller.js
+++ api/controllers/aut.controller.js
@@ -1,21 +1,43 @@
-import { errorhandler } from "../Utils/error.js"
-import User from "../models/user.model.js"
-// import bcryptjs from "bcryptjs"
+import { errorhandler } from "../Utils/error.js";
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
+export const signup = async (req, res, next) => {
+  console.log(req.body);
 
-export const  signup =async (req,res ,next)=>{
-console.log(req.body)
+  // res.send("send successfully")
+  const { username, email, password } = req.body;
+  const hashedpass = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedpass });
 
-// res.send("send successfully")
-const {username,email,password}=req.body
-// const hashedpass =  bcryptjs.hashSync(password,10)
-const newUser = new User ({username,email,password })
-
-try{
-    await newUser.save()
-res.status(201).json("user created successfully")
-} catch(err){
+  try {
+    await newUser.save();
+    res.status(201).json("user created successfully");
+  } catch (err) {
     next(err);
-}
-}
- 
+    console.log(err);
+  }
+};
+
+// sign in function////////////////////
+
+export const signin = async (req, res, next) => {
+    console.log(req.body)
+  const { email, password } = req.body;
+  try {
+    const validuser = await User.findOne({ email });
+    console.log(validuser)
+    if (!validuser) return next(errorhandler(404, "User not found!"));
+    const validPassword = bcryptjs.compareSync(password, validuser.password);
+    if (!validPassword) return next(errorhandler(401, "Invalid Password!"));
+    const token = jwt.sign({ id: validuser._id }, process.env.JWT_SECRET);
+    const {password:pass,...rest}=validuser._doc
+    res
+      .cookie("access_Token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (err) {
+    next(err);
+  }
+};
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,7 +1,10 @@
 import express from 'express';
-import { signup } from '../controllers/aut.controller.js';
+import {signin, signup } from '../controllers/aut.controller.js';
 
 const router=express.Router()
 
 router.post("/signup",signup)
+
+router.get("/signin",signin)
+
 export default router; 
