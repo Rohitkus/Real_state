aa20343deb1e20bf154357a8e845b8169e449588
Rohitkus <79927890+Rohitkus@users.noreply.github.com>
2024-03-05 08:59:10 +0530

    create redux toolkit

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,21 +4,21 @@ import {Link} from "react-router-dom"
 
 export default function Header() {
   return (
-    <header className="bg-slate-300 shadow-md">
-      <div className="flex justify-between item-center max-w-4xl m-auto p-1 ">
+    <header className="bg-slate-300 shadow-md ">
+      <div className="flex justify-between  items-center max-w-6xl m-auto p-1   ">
         {/* logo */}
         <Link to="/">
-        <h1 className="font-bold text-sm sm:text-xl flex flex-wrap items-center">
+        <h1 className="font-bold text-sm sm:text-xl flex flex-wrap justify-center item-center">
           <span className="text-slate-500">Rohit</span>
           <span className="text-slate-5800">Estate</span>
         </h1></Link>
         {/* search */}
-        <form className="bg-slate-100 rounded-lg p-1 flex items-center ">
-          <input type="text" placeholder="Search... "  className="bg-transparent focus:outline-none w-24 sm:w-48" />
+        <form className="bg-slate-100 rounded-lg p-1 flex  justify-center   items-center   ">
+          <input type="text" placeholder="Search... "  className="bg-transparent focus:outline-none w-24 sm:w-48 " />
           <FaSearch className="text-slate-600" />
         </form>
         {/* nav links */}
-        <ul className="flex gap-5  text-slate-700">
+        <ul className="flex gap-5 justify-center items-center  text-slate-700">
             <Link to="/">
             <li className="hidden md:inline hover:underline">Home</li></Link>
             <Link to="/about"> <li className="hidden md:inline hover:underline">About</li></Link>
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
 import {Link ,useNavigate} from 'react-router-dom'
-
+import  { useDispatch, useSelector } from 'react-redux'
+import {signInStart,signInFailure,signInsuccess} from '../redux/user/userSlice.js'
+ 
 export default function Signin() {
   const [formData, setFormData] = useState({});
-  const [error,setError]=useState(null)
-  const [loading,setLoading] = useState(false)
+  const { loading , error }= useSelector((state)=>state.user)
   const navigate= useNavigate()
+  const dispatch=useDispatch()
     
  const handleChange = (e) => {
     setFormData({
@@ -18,7 +20,7 @@ export default function Signin() {
   e.preventDefault();
   try{ 
     
-    setLoading(true)
+   dispatch(signInStart())
      const res = await fetch('/api/auth/signin', {
        method: 'POST',
        headers: {
@@ -29,18 +31,14 @@ export default function Signin() {
      const data= await res.json()
      console.log(data)
      if(data.success === false){
-       setError(data.message);
-       setLoading(false)
+    dispatch(signInFailure(data.message))
        return
      }
-     setLoading(false)
-     setError(null)
+     dispatch(signInsuccess(data))
      navigate('/')
 
   }  catch(error){
-    setLoading(false)
-    setError(error.message)
-
+    dispatch(signInFailure(error.message))
   }
 
  }
