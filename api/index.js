1ed099367d95a9519a821f22e94287b6d3ed268e
Rohitkus <79927890+Rohitkus@users.noreply.github.com>
2024-03-07 18:45:16 +0530

    complete image upload functionality

--- api/index.js
+++ api/index.js
@@ -21,6 +21,9 @@ app.listen(3000,()=>{console.log(
 
 app.use('/api/user',userRouter)
 app.use('/api/auth',authRouter)
+
+// middleware for error handling///////
+
 app.use((err, req,res,next)=>{
     const statusCode=err.statusCode ||500;
     const message= err.message || "internal server error"
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,15 +1,71 @@
 import { useSelector } from "react-redux"
+import { useRef, useState ,useEffect} from "react"
+import {getStorage,ref,uploadBytesResumable,getDownloadURL} from 'firebase/storage'
+import {app} from '../firebase.js'
 
 export default function Profile() {
   const {currentUser}= useSelector((state)=>state.user)
+  const fileref= useRef(null)
+  const [file,setfile]=useState(undefined)
+  const [FormData,setFormData]=useState({})
+  const [fileper,setfileper]= useState(0)
+  const [fileUploadError,setFileUploadError]= useState(false)
+  console.log(FormData)
+
+  
+  console.log(fileper) 
+  console.log(fileUploadError)
+// /ffffffffffffffffff
+useEffect(()=>{
+  if(file){
+    handleFileUpload(file)
+  }
+},[file])
+const handleFileUpload=(file)=>{ 
+   const storage= getStorage(app)
+   const fileName= new Date().getTime()+file.name;
+   const storageRef= ref(storage,fileName);
+   const uploadTask = uploadBytesResumable(storageRef,file)
+  
+
+   uploadTask.on('state_changed', 
+   (snapshot)=>{
+    const progress = (snapshot.bytesTransferred /
+    snapshot.totalBytes)*100;
+    // console.log("upload is " + progress+" % done")
+    setfileper(Math.round(progress))
+    setFileUploadError(false)
+   },
+   (error)=>{
+     setFileUploadError(true)
+    
+   },
+   ()=>{
+    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL)=>setFormData({...FormData,avatar:downloadURL}))
+   }
+   );
+} 
+
   return (
     <div className="p-3 max-w-lg m-auto">
     <h1 className='text-center text-3xl font-semibold'>Profile</h1>
      
      <form>
-      <img src={currentUser.avatar} alt="profile"
+      <input type="file" ref={fileref} hidden accept='image/*'
+      onChange={(e)=>setfile(e.target.files[0])} />
+      <img src={FormData.avatar || currentUser.avatar} alt="profile"
        className="rounded-full w-24 h-24 object-cover cursor-pointer 
-       self-center m-auto mt-3"  />
+       self-center m-auto mt-3"  onClick={()=> fileref.current.click()} />
+    <p className="text-sm  p-1 flex items-center justify-center">{fileUploadError ?(
+    <span className="text-red-700">Error image upload</span>
+    ):fileper > 0 && fileper< 100 ?( <span>{`uplaoding ${fileper} %`}</span>):fileper===100 ?( 
+    <span className="text-green-700">Image Upload Successsfully </span> ):('')
+     
+   
+      
+      }</p>
+
+
        <input type="text" placeholder="Username"  className="border p-3
         rounded-lg w-full" id="username"/>
         <input type="email" placeholder="Email"  className="border p-3
@@ -21,8 +77,8 @@ export default function Profile() {
          text-white">Update</button>
 
          <div className="flex justify-between mt-5">
-          <span className="text-red-800 cursor-pointer font-semibold ">Delete Account</span> 
-          <span className="text-red-800 cursor-pointer font-semibold ">Sign Out</span> 
+          <span className="text-red-800 cursor-pointer font-semibold ">Delete account</span> 
+          <span className="text-red-800 cursor-pointer font-semibold ">Sign out</span> 
 
          </div>
      </form>
