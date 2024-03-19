df9c4853e3b180dc612b5c89cb520eea3d3edc41
Rohitkus <79927890+Rohitkus@users.noreply.github.com>
2024-03-19 23:37:25 +0530

    change  to render

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import  userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
 import cookieParser from "cookie-parser";
 import listingRoute from "./routes/listing.route.js"
+import path from 'path'
 dotenv.config();
 
 mongoose.connect (process.env.MONGO).then(()=>{
@@ -12,6 +13,8 @@ mongoose.connect (process.env.MONGO).then(()=>{
 }).catch((err)=>{
     console.log(err)
 });
+
+const __dirname= path.resolve();
  
 const app=express();
 app.use(express.json())
@@ -25,6 +28,11 @@ app.listen(3000,()=>{console.log(
 app.use('/api/user',userRouter)
 app.use('/api/auth',authRouter)
 app.use('/api/listing',listingRoute)
+app.use(express.static(path.join(__dirname,'/client/dist')));
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','dist','index.html'));
+})
+ 
 
 // middleware for error handling///////
 
